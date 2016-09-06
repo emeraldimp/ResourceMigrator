734a5c8b53f941bd6c93515b277ea93357b87b32
Kellen Lask <kellenlask@gmail.com>
2016-09-05 21:08:56 -0600

    Added iOS Localized String Support 	- Apparently iOS was missing string support, which was easy to 	  add. 	- Strings will now be added to the appropriate folders and files 	  when translated from resx

--- ResourceMigrator/Extensions.cs
+++ ResourceMigrator/Extensions.cs
@@ -9,65 +9,71 @@ namespace ResourceMigrator
 {
 	public static class Extensions
 	{
+        /// <summary>
+        /// Extension method for String class to conveniently create an escaped version of the string's contents.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Escaped representation of the string</returns>
 		public static string ToEscapedString(this string input)
 		{
+            // Iterate through the string character by character looking for characters that need to be escaped. 
 			var literal = new StringBuilder(input.Length + 2);
 			foreach (var c in input)
 			{
 				switch (c)
 				{
-					case '\'':
+					case '\'':  // Quote
 						literal.Append(@"\'");
 						break;
 
-					case '\"':
+					case '\"':  // Double Quote
 						literal.Append("\\\"");
 						break;
 
-					case '\\':
+					case '\\':  // Backslash
 						literal.Append(@"\\");
 						break;
 
-					case '\0':
+					case '\0':  // Unicode
 						literal.Append(@"\0");
 						break;
 
-					case '\a':
+					case '\a':  // Alert
 						literal.Append(@"\a");
 						break;
 
-					case '\b':
+					case '\b':  // Backspace
 						literal.Append(@"\b");
 						break;
 
-					case '\f':
+					case '\f':  // Form Feed
 						literal.Append(@"\f");
 						break;
 
-					case '\n':
+					case '\n':  // New Line
 						literal.Append(@"\n");
 						break;
 
-					case '\r':
+					case '\r':  // Return
 						literal.Append(@"\r");
 						break;
 
-					case '\t':
+					case '\t':  // Tab
 						literal.Append(@"\t");
 						break;
 
-					case '\v':
+					case '\v':  // Vertical Quote
 						literal.Append(@"\v");
 						break;
 
-					default:
+					default:    // No escape needed (but could be a unicode character)
 						if (char.GetUnicodeCategory(c) != UnicodeCategory.Control)
 						{
 							literal.Append(c);
 						} 
 						else
 						{
-							literal.Append(@"\u");
+							literal.Append(@"\u");  // Handle unicode
 							literal.Append(((ushort)c).ToString("x4"));
 						}
 						break;
@@ -77,28 +83,49 @@ namespace ResourceMigrator
 		}
 
 
+        /// <summary>
+        /// FileSystemInfo extension class to conveniently parse resx file into Dictionary of string - string
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
 		public static Dictionary<string, string> LoadResources(this FileSystemInfo file)
 		{
-			if (file == null) throw new ArgumentNullException("file");
-			var result = new Dictionary<string, string>();
+            // Should never be hit, but you never know...
+            if (file == null)
+            {
+                throw new ArgumentNullException("File was null! @ Extensions.LoadResource(FileSystemInfo)");
+            }
 
+			var returnDict = new Dictionary<string, string>();
+
+            // Parse the resx file as XML 
 			var doc = new XmlDocument();
 			doc.Load(file.FullName);
 
+            // Grab the data node containing the resx key-value pairs
 			var nodes = doc.SelectNodes("//data");
 
 			if (nodes != null)
+                // Add each key-value pair to the dictionary
 				foreach (XmlNode node in nodes)
 				{
 					if (node.Attributes == null) continue;
 					var name = node.Attributes["name"].Value;
 					var value = node.ChildNodes[1].InnerText;
-					result.Add(name, value);
+					returnDict.Add(name, value);
 				}
 
-			return result;
+            // return the key-value pairs
+			return returnDict;
 		}
 
+
+        /// <summary>
+        /// FileSystemInfo extension method to conveniently parse the name of the file in question
+        /// into a resource type -- most useful for Android resource types, but still used by iOS.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
 		public static string GetResourceType(this FileSystemInfo fileName)
 		{
 			switch (fileName.Name.ToLower().Substring(0, Math.Min(3, fileName.Name.Length)))
