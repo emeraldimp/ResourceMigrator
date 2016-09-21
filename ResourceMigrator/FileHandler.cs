b0cad9aaffa7fd526fe54f86b0beacf556f5f031
Kellen Lask <kellenlask@Kellens-MacBook-Pro.local>
2016-09-21 17:28:46 -0600

    Fixes 	- Grabs Android projects by 'Droid' and 'Android' 	- If no solution directory is supplied, uses the executable's directory

--- ResourceMigrator/FileHandler.cs
+++ ResourceMigrator/FileHandler.cs
@@ -3,88 +3,118 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 
+
 namespace ResourceMigrator
 {
-	public static class FileHandler
-	{
-		private static readonly string _iosUniqueIdentifier = "iOS";
-		private static readonly string _androidUniqueIdentifier = "Droid";
-		private static readonly string _pclUniqueIdentifier = "Portable";
+    public static class FileHandler
+    {
+        private static readonly string _iosUniqueIdentifier = "iOS";
+        private static readonly string _androidUniqueIdentifier1 = "Droid";
+        private static readonly string _androidUniqueIdentifier2 = "Android";
+        private static readonly string _pclUniqueIdentifier = "Portable";
+        private static readonly string _testUniqueIdentifier = "Test";
 
 
-		public static IList<ProjectModel> GetProjects(SolutionParser solution, string solutionPath)
-		{
+        public static IList<ProjectModel> GetProjects(SolutionParser solution, string solutionPath)
+        {
             // Our return list of projects
-			var projects = new List<ProjectModel>();
+            var projects = new List<ProjectModel>();
 
             // Iterate through the solution's list of projects looking for iOS/PCL/Android projects
-			foreach (var proj in solution.Projects)
-			{
-				var xmldoc = new XmlDocument();
-				try
-				{
-					xmldoc.Load(Path.Combine(solutionPath, proj.RelativePath));
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(ex.Message);
-				}
+            foreach (var proj in solution.Projects)
+            {
+                // Exclude test projects
+                if (proj.ProjectName.Contains(_testUniqueIdentifier))
+                {
+                    continue;
+                }
 
-				var mgr = new XmlNamespaceManager(xmldoc.NameTable);
-				mgr.AddNamespace("x", "http://schemas.microsoft.com/developer/msbuild/2003");
+                var xmldoc = new XmlDocument();
+                try
+                {
+                    xmldoc.Load(Path.Combine(solutionPath, proj.RelativePath));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
 
-				var elemList = xmldoc.GetElementsByTagName("Import");
-				for (var i = 0; i < elemList.Count; i++)
-				{
-					var xmlAttributeCollection = elemList[i].Attributes;
-					if (xmlAttributeCollection == null) continue;
+                var mgr = new XmlNamespaceManager(xmldoc.NameTable);
+                mgr.AddNamespace("x", "http://schemas.microsoft.com/developer/msbuild/2003");
 
-					var attrVal = xmlAttributeCollection["Project"].Value;
-					var projectPath = Path.Combine(solutionPath, proj.RelativePath).Replace(proj.ProjectName + ".csproj", "");
+                var elemList = xmldoc.GetElementsByTagName("Import");
+                for (var i = 0; i < elemList.Count; i++)
+                {
+                    var xmlAttributeCollection = elemList[i].Attributes;
+                    if (xmlAttributeCollection == null)
+                    {
+                        continue;
+                    }
 
-					if (attrVal.Contains(_iosUniqueIdentifier))
-					{
-						projects.Add(new ProjectModel { ProjectNamespace = proj.ProjectName, ProjectPath = projectPath, PlatformType = PlatformType.Ios });
-					}
-                    else if (attrVal.Contains(_androidUniqueIdentifier))
-					{
-						projects.Add(new ProjectModel { ProjectNamespace = proj.ProjectName, ProjectPath = projectPath, PlatformType = PlatformType.Droid });
-					}
-                    else if (attrVal.Contains(_pclUniqueIdentifier))
-					{
-						projects.Add(new ProjectModel { ProjectNamespace = proj.ProjectName, ProjectPath = projectPath, PlatformType = PlatformType.Pcl });
-					}
-				}
-			}
-			return projects;
-		}
+                    var attrVal = xmlAttributeCollection["Project"].Value;
+                    var projectPath = Path.Combine(solutionPath, proj.RelativePath)
+                        .Replace(proj.ProjectName + ".csproj", "");
+
+                    if (attrVal.Contains(_iosUniqueIdentifier))
+                    {
+                        projects.Add(new ProjectModel {
+                            ProjectNamespace = proj.ProjectName,
+                            ProjectPath = projectPath,
+                            PlatformType = PlatformType.Ios
+                        });
+                        break;
+                    }
+
+                    if (attrVal.Contains(_androidUniqueIdentifier1) || attrVal.Contains(_androidUniqueIdentifier2))
+                    {
+                        projects.Add(new ProjectModel {
+                            ProjectNamespace = proj.ProjectName,
+                            ProjectPath = projectPath,
+                            PlatformType = PlatformType.Droid
+                        });
+                        break;
+                    }
+
+                    if (attrVal.Contains(_pclUniqueIdentifier))
+                    {
+                        projects.Add(new ProjectModel {
+                            ProjectNamespace = proj.ProjectName,
+                            ProjectPath = projectPath,
+                            PlatformType = PlatformType.Pcl
+                        });
+                        break;
+                    }
+                }
+            }
+            return projects;
+        }
 
 
-		public static IEnumerable<string> GetAllResourceFiles(string solutionPath)
-		{
-			return Directory.GetFiles(solutionPath, "*.resx", SearchOption.AllDirectories);
-		}
+        public static IEnumerable<string> GetAllResourceFiles(string solutionPath)
+        {
+            return Directory.GetFiles(solutionPath, "*.resx", SearchOption.AllDirectories);
+        }
 
 
-		public static SolutionParser GetSolutionFromPath(string solutionPath)
-		{
-			var files = Directory.GetFiles(solutionPath, "*.sln");
-			return new SolutionParser(files[0]);
-		}
+        public static SolutionParser GetSolutionFromPath(string solutionPath)
+        {
+            var files = Directory.GetFiles(solutionPath, "*.sln");
+            return new SolutionParser(files[0]);
+        }
 
 
         /// <summary>
-        /// Writes a string to a file while also creating the directory structure to house the file
-        /// if any directories don't exist. 
+        ///     Writes a string to a file while also creating the directory structure to house the file
+        ///     if any directories don't exist.
         /// </summary>
         /// <param name="path">The path to the directory that will contain the file</param>
         /// <param name="fileName">The file's name</param>
         /// <param name="content">The content to write to the file</param>
-	    public static void WriteToFile(string path, string fileName, string content)
-	    {
-	        Directory.CreateDirectory(path);
+        public static void WriteToFile(string path, string fileName, string content)
+        {
+            Directory.CreateDirectory(path);
 
-	        File.WriteAllText(Path.Combine(path, fileName), content);
-	    }
-	}
+            File.WriteAllText(Path.Combine(path, fileName), content);
+        }
+    }
 }
--- ResourceMigrator/Program.cs
+++ ResourceMigrator/Program.cs
@@ -1,66 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
+
 
 // originally taken from http://stackoverflow.com/a/16987412/124069
 
 namespace ResourceMigrator
 {
-	public class Program
-	{
-		private static void Main(string[] args)
-		{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
             // Get the solution path from the execution arguments
-            var solutionPath = args[0];
+            var solutionPath = args.Length == 0 ? AppDomain.CurrentDomain.BaseDirectory : args[0];
 
             // Load the projects from the solution
             var solution = FileHandler.GetSolutionFromPath(solutionPath);
-			var projects = FileHandler.GetProjects(solution, solutionPath);
+            var projects = FileHandler.GetProjects(solution, solutionPath);
 
-			// Look for a PCL to pull resx files from 
-			var pcl = projects.FirstOrDefault(p => p.PlatformType == PlatformType.Pcl);
-			if (pcl == null) throw new Exception("Your resource files must be located in a PCL -- no PCL found.");
+            // Look for a PCL to pull resx files from 
+            var pcl = projects.FirstOrDefault(p => p.PlatformType == PlatformType.Pcl);
+            if (pcl == null)
+            {
+                throw new Exception("Your resource files must be located in a PCL -- no PCL found.");
+            }
 
-			// Find the platform-specific projects 
-			var windows = projects.Where(p => p.PlatformType == PlatformType.Windows).ToList();
-		    var ios = projects.Where(p => p.PlatformType == PlatformType.Ios).ToList();
-			var droid = projects.Where(p => p.PlatformType == PlatformType.Droid).ToList();
+            // Find the platform-specific projects 
+            var windows = projects.Where(p => p.PlatformType == PlatformType.Windows).ToList();
+            var ios = projects.Where(p => p.PlatformType == PlatformType.Ios).ToList();
+            var droid = projects.Where(p => p.PlatformType == PlatformType.Droid).ToList();
 
-			// Grab the PCL's resx files 
-			var resourceFiles = FileHandler.GetAllResourceFiles(pcl.ProjectPath);
+            // Grab the PCL's resx files 
+            var resourceFiles = FileHandler.GetAllResourceFiles(pcl.ProjectPath);
 
-			// For each resx file, translate the contents into native representations
-			foreach (var file in resourceFiles)
-			{
-				var fileInfo = new FileInfo(file);
-				var resources = fileInfo.LoadResources();
+            // For each resx file, translate the contents into native representations
+            foreach (var file in resourceFiles)
+            {
+                var fileInfo = new FileInfo(file);
+                var resources = fileInfo.LoadResources();
 
-				// Create the Android resources
-				if (droid.Count > 0)
-				{
-				    foreach (var proj in droid)
-				    {
+                // Create the Android resources
+                if (droid.Count > 0)
+                {
+                    foreach (var proj in droid)
+                    {
                         new Droid().WriteToTarget(proj, resources, fileInfo);
-				    }
-				}
+                    }
+                }
 
-				// Create the iOS resources
-				if (ios.Count > 0)
-				{
-				    foreach (var proj in ios)
-				    {
+                // Create the iOS resources
+                if (ios.Count > 0)
+                {
+                    foreach (var proj in ios)
+                    {
                         new Touch().WriteToTarget(proj, resources, fileInfo);
-				    }
-				}
+                    }
+                }
 
-				// Create the Windows Phone resources
-				if (windows.Count > 0)
-				{
-					 //new Phone().WriteToTarget(phone, resources, fileInfo);
-				}
-			}
-		}
-	}
+                // Create the Windows Phone resources
+                if (windows.Count > 0)
+                {
+                    //new Phone().WriteToTarget(phone, resources, fileInfo);
+                }
+            }
+        }
+    }
 }
