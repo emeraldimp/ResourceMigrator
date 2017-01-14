4dba321b9abe8f03e6b8c55c866e335858b8aaa9
Kellen Lask <kellenlask@Kellens-MacBook-Pro.local>
2017-01-13 21:38:52 -0700

    Added Xamarin Studio Plugin Project Added and empty project for a Xamarin Studio Plugin. After reading some of the documentation here: https://developer.xamarin.com/guides/cross-platform/xamarin-studio/customizing-ide/extending_xamarin_studio_with_addins/
    it looks like it'll be easy to implement like for Visual Studio. All
    that really needs to happen is we need an entry point, menu entry and
    reference to the Class Library with the actual heavy-lifting code. --
    Assuming all runs well. Currently, there is an intermixing of Windows
    and Unix file path formats which is causing a crash. After the crash, we
    may have difficulties with the Solution Parsing.

---
+++ ResourceMigratorXamExtension/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"ResourceMigratorXamExtension",
+	Namespace = "ResourceMigratorXamExtension",
+	Version = "1.0"
+)]
+
+[assembly: AddinName("ResourceMigratorXamExtension")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("ResourceMigratorXamExtension")]
+[assembly: AddinAuthor("kellenlask")]
